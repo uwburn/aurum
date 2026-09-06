dd932c2dad2e92d3d2f849abecf12a476733747e
Michele Tibaldi <tiboburn@gmail.com>
2026-09-06 19:45:35 +0200

    Fix inclusions

--- include/aurum.h
+++ include/aurum.h
@@ -17,6 +17,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 /*
 * SYSTEM
--- src/software_serial.c
+++ src/software_serial.c
@@ -1,7 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay_basic.h>
-#include <stddef.h>
 
 #include "aurum_private.h"
 
